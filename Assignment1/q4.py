bf5b568f07af0ad0180d406de5c63a6d8da31a92
kwojcicki <kwojcicki@sympatico.ca>
2018-10-01 19:20:13 -0400

    changed up q4

--- Assignment1/q4.py
+++ Assignment1/q4.py
@@ -1,15 +1,14 @@
 import numpy as np
 
-tolerance = 0.01
-stepsize = 0.01  # E
 
 A = np.array([
-    [1, 2, 4, 1],
-    [2, 3, 5, 1],
-    [3, 4, 6, 1]
-]).T
+    [1, 2, 3],
+    [2, 3, 4],
+    [4, 5, 6],
+    [1, 1, 1]
+])
 
-b = np.array([1, 1, 1, 1])
+b = np.array([1, 1, 1, 1]).T
 
 # Some complaining TODO: michael look at if this error is cause of something we should fix
 # C:/Users/kwojc/PycharmProjects/COMP4107/Assignment1/q4.py:15: FutureWarning: `rcond` parameter will change to the
--- Assignment1/q4tables.py
+++ Assignment1/q4tables.py
@@ -10,30 +10,33 @@ A = np.array([
     [1, 1, 1]
 ])
 
-b = np.array([1, 1, 1, 1])
+b = np.array([1, 1, 1, 1]).T
 
 
 def t(x):
-    try:
-        return np.dot(A.T, np.dot(A, x)) - np.dot(A.T, b)
-    except RuntimeWarning:
-        print("Run time warning: ", x)
-        return x
+    # try:
+    return np.dot(A.T, np.dot(A, x)) - np.dot(A.T, b)
+    # except RuntimeWarning:
+    #    print("Run time warning: ", x)
+    #    return x
 
 
-ix = x = np.random.rand(3)
-
-for stepsize in [0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5]:
+ix = np.random.rand(3)
+np.seterr(all='raise')
+for stepSize in [0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5]:
     x = ix
     iterations = 0
     while np.linalg.norm(t(x), 2) > tolerance:
         try:
+            # print(x)
+            # print(t(x))
+            x = x - np.dot(stepSize, t(x))
+        except FloatingPointError:
+            print("Error")
             print(x)
             print(t(x))
-            x = x - np.dot(stepsize, t(x))
-        except RuntimeWarning:
             break
         iterations += 1
-    table.append([stepsize, iterations, x])
+    table.append([stepSize, iterations, x])
 
 print(table)
