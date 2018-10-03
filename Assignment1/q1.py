a547ae6369e9a8518745a90431fbde7f525e45c5
michael <michaelkuang97@gmail.com>
2018-10-03 16:24:36 -0400

    c

--- Assignment1/q1.py
+++ Assignment1/q1.py
@@ -10,18 +10,44 @@ A = [
 ]
 
 u, s, v = np.linalg.svd(A)
-print(u)
-u = [i[:2] for i in u]
+k=2
+print('u:',u)
+u = [i[:k] for i in u]
 print(u)
 print(s)
-s = np.multiply(s, np.identity(4))
-print(s)
-s = [i[:2] for i in s]
-v = [i[:2] for i in v.T]
-print(v)
+s = np.diag(s[:k])
+print('s',s)
+v = [i[:k] for i in v.T]
+print('v',v)
 
-sv = np.dot(s[:2], u[3])
+sv = np.dot(s, u[3])
 usv = np.dot(v[0], sv)
 
 prediction = 4 + usv
 print("prediction is: %.2f" % prediction)
+
+Alice = [5,3,4,4]
+Alicia = [3,1,2,3]
+Bob = [4,3,4,3]
+## Alice2D
+Alice2D = np.dot(np.dot(Alice, v),np.linalg.inv(s))
+Alice2D=[0.64, -0.30]
+
+#using Alice2D to predict others
+sv = np.dot(s, u[0])
+usv = np.dot(Alice2D, sv)
+prediction = 4 + usv
+print("prediction is: %.2f" % prediction)
+
+Alicia2D = np.dot(np.dot(Alicia, v),np.linalg.inv(s))
+Bob2D = np.dot(np.dot(Bob, v),np.linalg.inv(s))
+
+print('Alice2d:', Alice2D)
+print('Alicia2d:', Alicia2D)
+print('Bob2d:', Bob2D)
+####find alice 4d
+# u, s, v = np.linalg.svd(A)
+# v = v.T
+# print(v)
+# Alice4d = np.dot(np.dot(Alice, v), np.linalg.inv(s)
+# print(Alice4d)
