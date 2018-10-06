bac8765d5d20604f42cb278766f718da5867e5dc
kwojcicki <kwojcicki@sympatico.ca>
2018-10-06 01:39:13 -0400

    added pretty graphs

--- Assignment1/q7.py
+++ Assignment1/q7.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 ml_dir = './ml-latest-small'
 ml_100k_dir='./ml-100k'
@@ -47,6 +48,15 @@ def show_graph(results):
     plt.show()
 
 
+def show_graph_put(results):
+    for result in results:
+        x = [i[0] for i in result[1]]
+        y = [j[1] for j in result[1]]
+        plt.plot(x, y, label=result[0])
+    plt.xlabel('Basis size')
+    plt.ylabel('Throughput (predictions/sec)')
+    plt.legend()
+    plt.show()
 k = 14
 ratings, tuples, total = read(ml_100k_dir+'/u.data')
 
@@ -68,6 +78,7 @@ for m in range(len(movie_averages)):
 # !!!! END EXPERIMENTAL !!!
 
 resultGraph = []
+timeGraph = []
 for split_percentage in list([0.2, 0.5, 0.8]):
     subset = np.random.choice(total, int(total * split_percentage), replace=False)
     train_set = []
@@ -84,9 +95,10 @@ for split_percentage in list([0.2, 0.5, 0.8]):
             test_data[t[0]][t[1]] = ratings[t[0]][t[1]]
 
     results = []
+    times = []
     for b in list([600, 650, 700, 750, 800, 850, 900, 943]):
         threshold_size = b  # from research paper
-
+        start = time.time()
         u, s, v = np.linalg.svd(train_data[:threshold_size])
         uk = u[:,:k]
         sk = np.diag(s[:k])
@@ -107,10 +119,14 @@ for split_percentage in list([0.2, 0.5, 0.8]):
         print("Test", len(test_set))
         print("Train", len(train_set))
         mae = mean_absolute_error(test_data[:943], pred_table, user_averages)
-
+        end = time.time()
         print(mae)
         print(uk.shape)
         results.append((b, mae))
+        times.append((b, len(test_set) / (end - start)))
+
     resultGraph.append((split_percentage, results))
+    timeGraph.append((split_percentage, times))
 
 show_graph(resultGraph)
+show_graph_put(timeGraph)
