c3c3ac47153b06343b47e484df91ce9f473d1e54
michael <michaelkuang97@gmail.com>
2018-10-21 15:54:44 -0400

    it kind works now :D

--- Assignment2/q1_rewrite.py
+++ Assignment2/q1_rewrite.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def model(x, hidden_dim = 8):
     input_dim = 2 # we got x and y as our inputs
     output_dim = 1 # just one value as output
-    stdev = 0.5
+    stdev = 0.8
     with tf.variable_scope('FunctionApproximator'):
         w_h1 = tf.get_variable('w_h1', shape=[input_dim, hidden_dim], initializer=tf.random_normal_initializer(stddev=stdev))
         b_h1 = tf.get_variable('b_h1', shape=[hidden_dim], initializer=tf.constant_initializer(0.))
@@ -19,9 +19,6 @@ def model(x, hidden_dim = 8):
 
 
 # Function we are approximating
-# def f(input):
-#     print(input[0], input[1])
-#     return np.cos(input[0] + 6 * 0.35 * input[1]) + 2 * 0.35 * input[0] * input[1]
 def f(x, y):
     return np.cos(x + 6 * 0.35 * y) + 2 * 0.35 * x * y
 
@@ -41,65 +38,56 @@ def generate_data():
     # return training inputs, training outputs, testing inputs, testing outputs
     return myList[:100], labels[:100], myList[100:], labels[100:]
 
-
+#Generate our dataset
 trX, trY, teX, teY = generate_data()
-print(trX)
-with tf.variable_scope('Graph') as scope:
-    x = tf.placeholder("float", shape=[None, 2], name='inputs')
-    y_true = tf.placeholder("float", shape=[None, 1], name='y_true')
+
+
+for size in [2,8,50]:
+    tf.reset_default_graph()
+    print("Training with {} number of hidden neurons".format(size))
+    with tf.variable_scope('Graph') as scope:
+        x = tf.placeholder("float", shape=[None, 2], name='inputs')
+        y_true = tf.placeholder("float", shape=[None, 1], name='y_true')
+        #output of our model
+        y_pred = model(x, hidden_dim=size)
+        with tf.variable_scope('Loss'):
+            loss = tf.reduce_mean(tf.square(y_true - y_pred))
+        train_op = tf.train.GradientDescentOptimizer(learning_rate=0.3).minimize(loss)
+        predict_op = y_pred
     
-    #output of our model
-    y_pred = model(x, hidden_dim=8)
-    
-    with tf.variable_scope('Loss'):
-        loss = tf.reduce_mean(tf.square(y_true - y_pred))
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
-    train_op = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(loss)
-    predict_op = y_pred
-
-saver = tf.train.Saver()
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-
-    #training
-    print(len(trX))
-    for i in range(100):
-        for start, end in zip(range(0, len(trX), 10), range(10, len(trY) + 1, 10)):
-            curr_loss, _ = sess.run([loss, train_op], feed_dict={x:trX[start:end], y_true: trY[start:end]})
-            predicted = sess.run(predict_op, feed_dict={x:trX[start:end]})
-        print("Epoch {}: Loss: {}".format(i, curr_loss))
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    u = np.linspace(-1, 1, 5)
-    x1, y1 = np.meshgrid(u, u)
-
-    #cs = ax.contour(x1, y1, f(x1, y1))
-    #plt.clabel(cs, fontsize=10, colors=plt.cm.Reds(cs.norm(cs.levels)))
-    #plt.colorbar(cs)
-    #plt.show()
+        for i in range(100):
+            for start, end in zip(range(0, len(trX), 10), range(10, len(trY) + 1, 10)):
+                curr_loss, _ = sess.run([loss, train_op], feed_dict={x:trX[start:end], y_true: trY[start:end]})
+                predicted = sess.run(predict_op, feed_dict={x:trX[start:end]})
+            print("Epoch {}: Loss: {}".format(i, curr_loss))
+        
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        u = np.linspace(-1, 1, 5)
+        x1, y1 = np.meshgrid(u, u)
+        #cs = ax.contour(x1, y1, f(x1, y1))
+        #plt.clabel(cs, fontsize=10, colors=plt.cm.Reds(cs.norm(cs.levels)))
+        #plt.colorbar(cs)
+        #plt.show()
 
 
-    # print(x, y)
-    com = np.vstack((x1.flatten(), y1.flatten())).T
-    teX = []
-    for i in range(len(com)):
-        print(com[i])
-        teX.append(com[i])
-    print('teX',teX)
-    print('com',com)
-    predicted = sess.run(predict_op, feed_dict={x: com})
-    predicted = np.reshape(predicted, (-1, 5))
-    print(predicted)
-    # print(x)
-    # for i in com:
-    #     print(f(i[0],i[1]))
-    cs = ax.contour(x1, y1, predicted)
-    plt.clabel(cs, fontsize=10, colors=plt.cm.Reds(cs.norm(cs.levels)))
-    plt.colorbar(cs)
-    plt.show()
-    # plt.clabel(cs, fontsize=10, colors=plt.cm.Reds(cs.norm(cs.levels)))
+        com = np.vstack((x1.flatten(), y1.flatten())).T
+        predicted = sess.run(predict_op, feed_dict={x: com})
+        predicted = np.reshape(predicted, (-1, 5))
+        
+        cs = ax.contour(x1, y1, predicted)
+        plt.clabel(cs, fontsize=10, colors=plt.cm.Reds(cs.norm(cs.levels)))
+        plt.colorbar(cs)
+
+
+cs = ax.contour(x1, y1, f(x1, y1))
+plt.colorbar(cs)
+plt.show()
+        # plt.clabel(cs, fontsize=10, colors=plt.cm.Reds(cs.norm(cs.levels)))
 
 
 
