8f4c8b2ce1e845c5bb7e9a0889c5b9b4b04d8b07
michael <michaelkuang97@gmail.com>
2018-10-22 11:44:31 -0400

    q1 added early stopping

--- Assignment2/q1_rewrite.py
+++ Assignment2/q1_rewrite.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 def model(x, hidden_dim = 8):
     input_dim = 2 # we got x and y as our inputs
     output_dim = 1 # just one value as output
-    stdev = 2
+    stdev = 1
     with tf.variable_scope('FunctionApproximator'):
         w_h1 = tf.get_variable('w_h1', shape=[input_dim, hidden_dim], initializer=tf.random_normal_initializer(stddev=stdev))
         b_h1 = tf.get_variable('b_h1', shape=[hidden_dim], initializer=tf.constant_initializer(0.))
 
-        z = tf.nn.sigmoid(tf.matmul(x, w_h1) + b_h1)
+        z = tf.nn.tanh(tf.matmul(x, w_h1) + b_h1)
 
         w_o = tf.get_variable('w_o', shape=[hidden_dim, output_dim], initializer=tf.random_normal_initializer(stddev=stdev))
 
@@ -60,11 +60,19 @@ for size in [2, 8, 50]:
         sess.run(tf.global_variables_initializer())
 
         for i in range(100):
-            for start, end in zip(range(0, len(trX), 10), range(10, len(trY) + 1, 10)):
+            batch_size = 10
+            average_loss = 0
+            num_itr = 100/batch_size
+            for start, end in zip(range(0, len(trX), batch_size), range(batch_size, len(trY) + 1, batch_size)):
                 curr_loss, _ = sess.run([loss, train_op], feed_dict={x:trX[start:end], y_true: trY[start:end]})
                 predicted = sess.run(predict_op, feed_dict={x:trX[start:end]})
+                average_loss += curr_loss
+            average_loss /= num_itr
+            if average_loss < 0.02:
+                break
+                    
             # print("Epoch {}: Loss: {}".format(i, curr_loss))
-        print("{} Neurons results in a {} MSE".format(size,curr_loss))
+        print("{} Neurons results in a {} MSE, number of epochs: {}".format(size,curr_loss, i))
         
         fig = plt.figure()
         ax = fig.add_subplot(111)
@@ -78,8 +86,9 @@ for size in [2, 8, 50]:
 
         com = np.vstack((x1.flatten(), y1.flatten())).T
         predicted = sess.run(predict_op, feed_dict={x: com})
+        # print(test_loss)
         predicted = np.reshape(predicted, (-1, 5))
-        
+    
         cs = ax.contour(x1, y1, predicted)
         plt.clabel(cs, fontsize=10, colors=plt.cm.Reds(cs.norm(cs.levels)))
         plt.colorbar(cs)
