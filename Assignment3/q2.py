64a21c72d91461b37550010cdaee570b3dff051d
kwojcicki <kwojcicki@sympatico.ca>
2018-11-13 22:06:41 -0500

    not learning

--- Assignment3/q2.py
+++ Assignment3/q2.py
@@ -1,53 +1,48 @@
-import argparse
-import sys
 import sklearn.cluster as sk
+from sklearn.utils import shuffle
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
-import math
 
 
-# creates activation function
-def gaussian_function(input_layer):
-    initial = math.exp(-2*math.pow(input_layer, 2))
-    return initial
+def init_weights(shape):
+    return tf.Variable(tf.random_normal(shape, stddev=0.75))
 
-np_gaussian_function = np.vectorize(gaussian_function)
 
-def d_gaussian_function(input_layer):
-    initial = -4 * input_layer * math.exp(-2*math.pow(input_layer, 2))
-    return initial
+input_size = 784
+output_size = 10
+centroids = 100
+X = tf.placeholder("float", shape=[None, input_size])
+Y = tf.placeholder("float", shape=[None, output_size])
 
-np_d_gaussian_function = np.vectorize(d_gaussian_function)
+w_h1 = init_weights([centroids, output_size])
 
-np_d_gaussian_function_32 = lambda input_layer: np_d_gaussian_function(input_layer).astype(np.float32)
+def model(X, w, centroid, b):
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        b = tf.transpose(b)
+        #print("stack", sess.run(tf.tile(tf.expand_dims(X, 1), [1, 2, 1]), feed_dict={X: [[0, 0], [1, 1]]}))
+        #print("centroid", sess.run(tf.reshape(tf.tile(centroid, [2, 1]), [2, 2, 2]), feed_dict={X: [[0, 0], [1, 1]]}))
 
-def tf_d_gaussian_function(input_layer, name=None):
-    with ops.name_scope(name, "d_gaussian_function", [input_layer]) as name:
-        y = tf.py_func(np_d_gaussian_function_32, [input_layer],[tf.float32], name=name, stateful=False)
-    return y[0]
+        x1 = tf.to_float(tf.tile(tf.expand_dims(X, 1), [1, centroids, 1]))
+        centroid1 = tf.to_float(tf.reshape(tf.tile(centroid, [tf.shape(X)[0], 1]), [tf.shape(X)[0],
+                                                                                    centroids,
+                                                                                    input_size]))
 
-def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
-    rnd_name = 'PyFunGrad' + str(np.random.randint(0, 1E+8))
-
-    tf.RegisterGradient(rnd_name)(grad)
-    g = tf.get_default_graph()
-    with g.gradient_override_map({"PyFunc": rnd_name}):
-        return tf.py_func(func, inp, Tout, stateful=stateful, name=name)
-
-def gaussian_function_grad(op, grad):
-    input_variable = op.inputs[0]
-    n_gr = tf_d_gaussian_function(input_variable)
-    return grad * n_gr
-
-np_gaussian_function_32 = lambda input_layer: np_gaussian_function(input_layer).astype(np.float32)
-
-def tf_gaussian_function(input_layer, name=None):
-    with ops.name_scope(name, "gaussian_function", [input_layer]) as name:
-        y = py_func(np_gaussian_function_32, [input_layer], [tf.float32], name=name, grad=gaussian_function_grad)
-    return y[0]
-# end of defining activation function
-
+        #print("subtract", sess.run(tf.subtract(x1, centroid1), feed_dict={X: [[0, 0], [1, 1]]}))
+        #print("subtract", sess.run(tf.norm(tf.subtract(x1, centroid1), axis=-1), feed_dict={X: [[0, 0], [1, 1]]}))
+        dist = tf.square(tf.norm(tf.subtract(x1, centroid1), axis=-1))
+        #print("weights inside", sess.run(w))
+        #print("dist", sess.run(dist, feed_dict={X: [[0, 0], [1, 1]]}))
+        #print("b", b)
+        negative = tf.to_float(tf.negative(b))
+        #print("negative", sess.run(negative, feed_dict={X: [[0, 0], [1, 1]]}))
+        beta = tf.multiply(negative, dist)
+        #print("beta", sess.run(beta, feed_dict={X: [[0, 0], [1, 1]]}))
+        exponent = tf.exp(beta)
+        #print("w", sess.run(w, feed_dict={X: [[0, 0], [1, 1]]}))
+        #print("exponent", sess.run(exponent, feed_dict={X: [[0, 0], [1, 1]]}))
+        return tf.matmul(exponent, w)
 
 
 class mnistDataset:
@@ -55,14 +50,21 @@ class mnistDataset:
         mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
         trX1, trY1, teX1, teY1 = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
 
-        self.trX = []
-        self.trY = []
-        for i in range(len(trX1)):
-            if np.array_equal(trY1[i], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]) or \
-                    np.array_equal(trY1[i], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]):
+        for i in range(0, len(trX1)):
+            image_vector = trX1[i]
+            trX1[i] = [j / 255.0 for j in image_vector]
 
-                self.trX.append(trX1[i])
-                self.trY.append(trY1[i])
+        for i in range(0, len(teX1)):
+            image_vector = teX1[i]
+            teX1[i] = [j / 255.0 for j in image_vector]
+
+        self.trX = trX1
+        self.trY = trY1
+        self.teX = teX1
+        self.teY = teY1
+
+        self.trX, self.trY = shuffle(self.trX, self.trY)
+        self.teX, self.teY = shuffle(self.teX, self.teY)
 
     def kmean(self):
         with tf.Session() as sess:
@@ -81,9 +83,72 @@ class mnistDataset:
                     tf.Summary.Value(tag="objective function", simple_value=kmeans.inertia_/10),
                 ]), k)
 
+    def getCentroids(self, k=centroids):
+        kmeans = sk.KMeans(n_clusters=k, init='random', n_init=1).fit(self.trX)
+        print("inertia", kmeans.inertia_)
+        return kmeans.cluster_centers_
 
-data = mnistDataset()
-data.kmean()
+    def getBetas(self, cluster_centers=None):
+        if cluster_centers is None:
+            cluster_centers = self.getCentroids()
+
+        sigma = np.zeros([len(cluster_centers)], dtype=np.float32)
+
+        means = cluster_centers
+        for x in self.trX:
+            best = float("inf")
+            index = -1
+            for y in range(0, len(means)):
+                d = np.linalg.norm(x - means[y])
+                if d < best:
+                    best = d
+                    index = y
+            sigma[index] += best
+
+        return tf.divide(1, tf.multiply(2., tf.square(sigma)))
+
+
+rbf = mnistDataset()
+c = rbf.getCentroids()
+py_x = model(X, w_h1, centroid=c,
+             b=rbf.getBetas(c))
+
+
+def printAccuracy(data, labels, size, sess):
+    accuracy = 0.
+    for start, end in zip(range(0, len(data), size), range(size, len(data) + 1, size)):
+        accuracy += np.mean(np.argmax(labels[start:end], axis=1) ==
+                            sess.run(predict_op, feed_dict={X: data[start:end]}))
+    return accuracy
+
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
+train_op = tf.train.AdamOptimizer().minimize(cost)
+predict_op = tf.argmax(py_x, 1)
+
+batchSize = 128
+with tf.Session() as sess:
+    summary_writer = tf.summary.FileWriter('logs/', graph=sess.graph)
+    tf.global_variables_initializer().run()
+    summary_writer.add_graph(py_x)
+    for i in range(20):
+        weightsBefore = sess.run(w_h1)
+        for start, end in zip(range(0, len(rbf.trX), batchSize), range(batchSize, len(rbf.trX)+1, batchSize)):
+            sess.run(train_op, feed_dict={X: rbf.trX[start:end], Y: rbf.trY[start:end]})
+
+        accuracy = printAccuracy(rbf.trX, rbf.trY, 100, sess)
+        print("accuracy # after", accuracy)
+        print("accuracy % after", i, accuracy / int((len(rbf.trY) / batchSize)))
+
+        accuracy = printAccuracy(rbf.teX, rbf.teY, 100, sess)
+        print("accuracy # after testing", accuracy)
+        print("accuracy % after testing", i, accuracy / int((len(rbf.teY) / batchSize)))
+
+        weightsAfter = sess.run(w_h1)
+        print("Difference in weights", np.sum(weightsBefore - weightsAfter))
+
+# data = mnistDataset()
+# data.kmean()
 
 # keep for now :)
 # kmeansDistance = kmeans.transform(self.trX)
