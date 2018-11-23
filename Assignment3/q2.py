fd1f72bb51785ca5a46a0e056cc50c62bdcfffcb
michael <michaelkuang97@gmail.com>
2018-11-23 13:26:20 -0500

    c

--- Assignment3/q2.py
+++ Assignment3/q2.py
@@ -18,7 +18,7 @@ Y = tf.placeholder("float", shape=[None, output_size])
 
 w_h1 = init_weights([centroids, output_size])
 
-def model(X, w, centroid, b, keep_prob=0.5):
+def model(X, w, centroid, b, keep_prob=0.5, use_dropout = True):
     #with tf.Session() as sess:
     #tf.global_variables_initializer().run()
     b = tf.transpose(b)
@@ -46,8 +46,11 @@ def model(X, w, centroid, b, keep_prob=0.5):
     exponent = tf.exp(beta)
     #print("w", sess.run(w, feed_dict={X: [[0, 0], [1, 1]]}))
     #print("exponent", sess.run(exponent, feed_dict={X: [[0, 0], [1, 1]]}))
-    dropout = tf.nn.dropout(w, keep_prob=keep_prob)
-    return tf.matmul(exponent, dropout)
+    if use_dropout:
+        dropout = tf.nn.dropout(w, keep_prob=keep_prob)
+        return tf.matmul(exponent, dropout)
+    else:
+        return tf.matmul(exponent, w)
 
 
 class mnistDataset:
@@ -57,8 +60,33 @@ class mnistDataset:
 
         self.data = np.append(trX1, teX1, axis=0)
         self.labels = np.append(trY1, teY1, axis=0)
+        print('shape of labels:', self.labels.shape)
+        self.data_separated = [[] for i in range(10)]
+        self.labels_separated = [[] for i in range(10)]
 
-        self.data, self.labels = shuffle(self.data, self.labels)
+        print(self.labels_separated)
+        print("length", len(self.data))
+        for i in range(len(self.data)):
+            data_label = np.where(self.labels[i]==1)[0][0] #find index of 1 in one_hot encoded label
+            self.data_separated[data_label].append(self.data[i]) #use label as index for data_separated
+            self.labels_separated[data_label].append(self.labels[i])
+        for i in range(10):
+            print(len(self.data_separated[i]), len(self.labels_separated[i]))
+        self.labels_separated = np.asarray(self.labels_separated)
+        
+        self.ordered_data = self.data_separated[0]
+        self.ordered_labels = self.labels_separated[0]
+        self.ordered_labels = [self.ordered_labels.extend(self.labels_separated[i]) for i in range(len(self.labels_separated))]
+        print('len', len(self.ordered_labels))
+        # self.ordered_labels = self.ordered_labels.flatten()
+        for i in range(1,10):
+            self.ordered_data = np.append(self.ordered_data, self.data_separated[i], axis=0)
+            # print(len(self.ordered_labels))
+            # print(self.labels_separated.shape
+            # self.ordered_labels = np.append(self.ordered_labels, self.labels_separated[i])
+        # self.data, self.labels = shuffle(self.data, self.labels)
+        print(self.ordered_data.shape)
+        print(self.ordered_labels.shape)
 
     def kmean(self):
         with tf.Session() as sess:
@@ -107,9 +135,6 @@ class mnistDataset:
         return tf.divide(1, tf.multiply(2., tf.square(sigma)))
 
 
-
-
-
 def printAccuracy(data, labels, size, sess):
     accuracy = 0.
     for start, end in zip(range(0, len(data), size), range(size, len(data) + 1, size)):
@@ -128,22 +153,25 @@ while True:
 k_fold_accuracy = 0
 k_folds = 10
 fold = 0
-for keep_prob in [0.5, 1.0]:
+for keep_prob in [0.75, 1.0]:
     #try with dropout and without
     rbf = mnistDataset()
     c = rbf.getCentroids()
     py_x = model(X, w_h1, centroid=c,
                 b=rbf.getBetas(c), keep_prob=keep_prob)
-    predict_op = tf.argmax(py_x, 1)
+
+    test_pred = model(X, w_h1, centroid=c,
+            b=rbf.getBetas(c), keep_prob=keep_prob, use_dropout=False)
+    predict_op = tf.argmax(test_pred, 1)
     y_true_cls = tf.argmax(Y, dimension=1)
     batch_accuracies = tf.placeholder("float", [None])
     num_batches = tf.placeholder("float")
     with tf.name_scope('accuracy'):
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y)) #for training so use py_x
         correct_prediction = tf.equal(predict_op, y_true_cls)
         acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         mean_accuracy = tf.reduce_mean(tf.cast(batch_accuracies, tf.float32))
-
+    
     train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
     # train_op = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
 
@@ -157,8 +185,8 @@ for keep_prob in [0.5, 1.0]:
     for train_index, test_index in KFold(n_splits=k_folds).split(rbf.data):
         fold += 1
         print(train_index, test_index)
-        trX, teX = rbf.data[train_index], rbf.data[test_index]
-        trY, teY = rbf.labels[train_index], rbf.labels[test_index]
+        trX, teX = rbf.ordered_data[train_index], rbf.ordered_data[test_index]
+        trY, teY = rbf.ordered_labels[train_index], rbf.ordered_labels[test_index]
         print(len(trX), len(trY))
         result_dir = './logs/attempt_{}/fold_{}_keep_prob_{}/'.format(attempt,fold, int(keep_prob*100))
         with tf.Session() as sess:
@@ -190,6 +218,7 @@ for keep_prob in [0.5, 1.0]:
                 
                 test_accuracy_summary, m_accuracy =  sess.run([merged, mean_accuracy], feed_dict={batch_accuracies:total_accuracy})
                 summary_writer.add_summary(test_accuracy_summary, i+1)
+                print("Epoch : {}, Test Acc: ".format(i+1, m_accuracy))
             k_fold_accuracy += m_accuracy #final accuracy
     k_fold_accuracy = k_fold_accuracy/k_folds
     print("K-fold cross validation accuracy: {}".format(k_fold_accuracy))
