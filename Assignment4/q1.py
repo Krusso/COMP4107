5fb7d35dbea84a78c02de9934c14a1c39342c39f
kwojcicki <kwojcicki@sympatico.ca>
2018-11-27 17:29:07 -0500

    added model

--- Assignment4/q1.py
+++ Assignment4/q1.py
@@ -1,10 +1,8 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
 import os
-import sys
 import tarfile
-from six.moves import urllib
+from urllib.request import urlretrieve
 
 # https://www.tensorflow.org/tutorials/images/deep_cnn
 # https://github.com/tensorflow/models/tree/master/tutorials/image/cifar10/
@@ -20,208 +18,6 @@ IMAGE_SIZE = 32
 NUM_CLASSES = 10
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
 NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
-
-
-def maybe_download_and_extract():
-    """Download and extract the tarball from Alex's website."""
-    dest_directory = DATA_DIR
-    if not os.path.exists(dest_directory):
-        os.makedirs(dest_directory)
-    filename = DATA_URL.split('/')[-1]
-    filepath = os.path.join(dest_directory, filename)
-    if not os.path.exists(filepath):
-        def _progress(count, block_size, total_size):
-            sys.stdout.write('\r>> Downloading %s %.1f%%' % (filename,
-                                                             float(count * block_size) / float(total_size) * 100.0))
-            sys.stdout.flush()
-
-        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
-        print()
-        statinfo = os.stat(filepath)
-        print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
-    extracted_dir_path = os.path.join(dest_directory, 'cifar-10-batches-bin')
-    if not os.path.exists(extracted_dir_path):
-        tarfile.open(filepath, 'r:gz').extractall(dest_directory)
-
-
-def distorted_inputs():
-    """Construct distorted input for CIFAR training using the Reader ops.
-    Returns:
-      images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
-      labels: Labels. 1D tensor of [batch_size] size.
-    Raises:
-      ValueError: If no data_dir
-    """
-    data_dir = os.path.join(DATA_DIR, 'cifar-10-batches-bin')
-    images, labels = distorted_inputs(data_dir=data_dir,
-                                      batch_size=batch_size)
-
-    return images, labels
-
-
-def _generate_image_and_label_batch(image, label, min_queue_examples,
-                                    batch_size, shuffle):
-    """Construct a queued batch of images and labels.
-    Args:
-      image: 3-D Tensor of [height, width, 3] of type.float32.
-      label: 1-D Tensor of type.int32
-      min_queue_examples: int32, minimum number of samples to retain
-        in the queue that provides of batches of examples.
-      batch_size: Number of images per batch.
-      shuffle: boolean indicating whether to use a shuffling queue.
-    Returns:
-      images: Images. 4D tensor of [batch_size, height, width, 3] size.
-      labels: Labels. 1D tensor of [batch_size] size.
-    """
-    # Create a queue that shuffles the examples, and then
-    # read 'batch_size' images + labels from the example queue.
-    num_preprocess_threads = 16
-    if shuffle:
-        images, label_batch = tf.train.shuffle_batch(
-            [image, label],
-            batch_size=batch_size,
-            num_threads=num_preprocess_threads,
-            capacity=min_queue_examples + 3 * batch_size,
-            min_after_dequeue=min_queue_examples)
-    else:
-        images, label_batch = tf.train.batch(
-            [image, label],
-            batch_size=batch_size,
-            num_threads=num_preprocess_threads,
-            capacity=min_queue_examples + 3 * batch_size)
-
-    # Display the training images in the visualizer.
-    tf.summary.image('images', images)
-
-    return images, tf.reshape(label_batch, [batch_size])
-
-
-def distorted_inputs(data_dir, batch_size):
-    """Construct distorted input for CIFAR training using the Reader ops.
-    Args:
-      data_dir: Path to the CIFAR-10 data directory.
-      batch_size: Number of images per batch.
-    Returns:
-      images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
-      labels: Labels. 1D tensor of [batch_size] size.
-    """
-    filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i)
-                 for i in range(1, 6)]
-    for f in filenames:
-        if not tf.gfile.Exists(f):
-            raise ValueError('Failed to find file: ' + f)
-
-    # Create a queue that produces the filenames to read.
-    filename_queue = tf.train.string_input_producer(filenames)
-
-    with tf.name_scope('data_augmentation'):
-        # Read examples from files in the filename queue.
-        read_input = read_cifar10(filename_queue)
-        reshaped_image = tf.cast(read_input.uint8image, tf.float32)
-
-        height = IMAGE_SIZE
-        width = IMAGE_SIZE
-
-        # Image processing for training the network. Note the many random
-        # distortions applied to the image.
-
-        # Randomly crop a [height, width] section of the image.
-        distorted_image = tf.random_crop(reshaped_image, [height, width, 3])
-
-        # Randomly flip the image horizontally.
-        distorted_image = tf.image.random_flip_left_right(distorted_image)
-
-        # Because these operations are not commutative, consider randomizing
-        # the order their operation.
-        # NOTE: since per_image_standardization zeros the mean and makes
-        # the stddev unit, this likely has no effect see tensorflow#1458.
-        distorted_image = tf.image.random_brightness(distorted_image,
-                                                     max_delta=63)
-        distorted_image = tf.image.random_contrast(distorted_image,
-                                                   lower=0.2, upper=1.8)
-
-        # Subtract off the mean and divide by the variance of the pixels.
-        float_image = tf.image.per_image_standardization(distorted_image)
-
-        # Set the shapes of tensors.
-        float_image.set_shape([height, width, 3])
-        read_input.label.set_shape([1])
-
-        # Ensure that the random shuffling has good mixing properties.
-        min_fraction_of_examples_in_queue = 0.4
-        min_queue_examples = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN *
-                                 min_fraction_of_examples_in_queue)
-        print('Filling queue with %d CIFAR images before starting to train. '
-              'This will take a few minutes.' % min_queue_examples)
-
-    # Generate a batch of images and labels by building up a queue of examples.
-    return _generate_image_and_label_batch(float_image, read_input.label,
-                                           min_queue_examples, batch_size,
-                                           shuffle=True)
-
-
-def read_cifar10(filename_queue):
-    """Reads and parses examples from CIFAR10 data files.
-    Recommendation: if you want N-way read parallelism, call this function
-    N times.  This will give you N independent Readers reading different
-    files & positions within those files, which will give better mixing of
-    examples.
-    Args:
-      filename_queue: A queue of strings with the filenames to read from.
-    Returns:
-      An object representing a single example, with the following fields:
-        height: number of rows in the result (32)
-        width: number of columns in the result (32)
-        depth: number of color channels in the result (3)
-        key: a scalar string Tensor describing the filename & record number
-          for this example.
-        label: an int32 Tensor with the label in the range 0..9.
-        uint8image: a [height, width, depth] uint8 Tensor with the image data
-    """
-
-    class CIFAR10Record(object):
-        pass
-
-    result = CIFAR10Record()
-
-    # Dimensions of the images in the CIFAR-10 dataset.
-    # See http://www.cs.toronto.edu/~kriz/cifar.html for a description of the
-    # input format.
-    label_bytes = 1  # 2 for CIFAR-100
-    result.height = 32
-    result.width = 32
-    result.depth = 3
-    image_bytes = result.height * result.width * result.depth
-    # Every record consists of a label followed by the image, with a
-    # fixed number of bytes for each.
-    record_bytes = label_bytes + image_bytes
-
-    # Read a record, getting filenames from the filename_queue.  No
-    # header or footer in the CIFAR-10 format, so we leave header_bytes
-    # and footer_bytes at their default of 0.
-    reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
-    result.key, value = reader.read(filename_queue)
-
-    # Convert from a string to a vector of uint8 that is record_bytes long.
-    record_bytes = tf.decode_raw(value, tf.uint8)
-
-    # The first bytes represent the label, which we convert from uint8->int32.
-    result.label = tf.cast(
-        tf.strided_slice(record_bytes, [0], [label_bytes]), tf.int32)
-
-    # The remaining bytes after the label represent the image, which we reshape
-    # from [depth * height * width] to [depth, height, width].
-    depth_major = tf.reshape(
-        tf.strided_slice(record_bytes, [label_bytes],
-                         [label_bytes + image_bytes]),
-        [result.depth, result.height, result.width])
-    # Convert from [depth, height, width] to [height, width, depth].
-    result.uint8image = tf.transpose(depth_major, [1, 2, 0])
-
-    return result
-
-
-from urllib.request import urlretrieve
 
 
 def cifar10(path=None):
@@ -320,10 +116,14 @@ def init_weights(shape):
     return tf.Variable(tf.random_normal(shape, stddev=0.01))
 
 
-def model(X, w, w_fc, w_o, p_keep_conv, p_keep_hidden):
-    l1a = tf.nn.relu(tf.nn.conv2d(X, w,  # l1a shape=(?, 28, 28, 32)
+def model1(X, p_keep_conv, p_keep_hidden):
+    w = init_weights([3, 3, 3, 32])  # 3x3x3 conv, 32 outputs
+    w_fc = init_weights([32 * 16 * 16, 625])  # FC 32 * 14 * 14 inputs, 625 outputs
+    w_o = init_weights([625, 10])  # FC 625 inputs, 10 outputs (labels)
+
+    l1a = tf.nn.relu(tf.nn.conv2d(X, w,  # l1a shape=(?, 32, 32, 32)
                                   strides=[1, 1, 1, 1], padding='SAME'))
-    l1 = tf.nn.max_pool(l1a, ksize=[1, 2, 2, 1],  # l1 shape=(?, 14, 14, 32)
+    l1 = tf.nn.max_pool(l1a, ksize=[1, 2, 2, 1],  # l1 shape=(?, 16, 16, 32)
                         strides=[1, 2, 2, 1], padding='SAME')
     l1 = tf.nn.dropout(l1, p_keep_conv)
 
@@ -337,44 +137,91 @@ def model(X, w, w_fc, w_o, p_keep_conv, p_keep_hidden):
     return pyx
 
 
+def model2(X, p_keep_conv, p_keep_hidden):
+    w = init_weights([3, 3, 3, 32])  # 3x3x3 conv, 32 outputs
+    w_1 = init_weights([3, 3, 32, 32])  # 3x3x32 conv, 32 outputs
+    w_fc = init_weights([32 * 8 * 8, 625])  # FC 32 * 14 * 14 inputs, 625 outputs
+    w_o = init_weights([625, 10])  # FC 625 inputs, 10 outputs (labels)
+
+    l1a = tf.nn.relu(tf.nn.conv2d(X, w,  # l1a shape=(?, 32, 32, 32)
+                                  strides=[1, 1, 1, 1], padding='SAME'))
+    l1 = tf.nn.max_pool(l1a, ksize=[1, 2, 2, 1],  # l1 shape=(?, 16, 16, 32)
+                        strides=[1, 2, 2, 1], padding='SAME')
+    l1 = tf.nn.dropout(l1, p_keep_conv)
+
+    l2a = tf.nn.relu(tf.nn.conv2d(l1, w_1,  # l2a shape=(?, 16, 16, 32)
+                                  strides=[1, 1, 1, 1], padding='SAME'))
+    l2 = tf.nn.max_pool(l2a, ksize=[1, 2, 2, 1],  # l2 shape=(?, 8, 8, 32)
+                        strides=[1, 2, 2, 1], padding='SAME')
+    l2 = tf.nn.dropout(l2, p_keep_conv)
+
+    l3 = tf.reshape(l2, [-1, w_fc.get_shape().as_list()[0]])  # reshape to (?, 14x14x32)
+    l3 = tf.nn.dropout(l3, p_keep_conv)
+
+    l4 = tf.nn.relu(tf.matmul(l3, w_fc))
+    l4 = tf.nn.dropout(l4, p_keep_hidden)
+
+    pyx = tf.matmul(l4, w_o)
+    return pyx
+
+
 trX, trY, teX, teY = cifar10(path='./tmp')
-print(len(trX[0]))
-print(trX[0])
-print(np.array(trX[0]).shape)
 
 X = tf.placeholder("float", [None, 32, 32, 3])
 Y = tf.placeholder("float", [None, 10])
 
-w = init_weights([3, 3, 3, 32])  # 3x3x1 conv, 32 outputs
-w_fc = init_weights([32 * 14 * 14, 625])  # FC 32 * 14 * 14 inputs, 625 outputs
-w_o = init_weights([625, 10])  # FC 625 inputs, 10 outputs (labels)
-
 p_keep_conv = tf.placeholder("float")
 p_keep_hidden = tf.placeholder("float")
-py_x = model(X, w, w_fc, w_o, p_keep_conv, p_keep_hidden)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
-train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
-predict_op = tf.argmax(py_x, 1)
+attempt = 0
 
-# Launch the graph in a session
-with tf.Session() as sess:
-    # you need to initialize all variables
-    tf.global_variables_initializer().run()
+while True:
+    if os.path.isdir('./logs/attempt_{}'.format(attempt)):
+        attempt += 1
+    else:
+        break
 
-    for i in range(15):
-        training_batch = zip(range(0, len(trX), batch_size),
-                             range(batch_size, len(trX) + 1, batch_size))
-        for start, end in training_batch:
-            sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end],
-                                          p_keep_conv: 0.8, p_keep_hidden: 0.5})
+for py_x in list([model2(X, p_keep_conv, p_keep_hidden)]):
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
+    train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+    predict_op = tf.argmax(py_x, 1)
 
-        test_indices = np.arange(len(teX))  # Get A Test Batch
-        np.random.shuffle(test_indices)
-        test_indices = test_indices[0:test_size]
+    batch_accuracies = tf.placeholder("float", [None])
+    mean_accuracy = tf.reduce_mean(tf.cast(batch_accuracies, tf.float32))
+    tf.summary.scalar('accuracy', mean_accuracy)
 
-        print(i, np.mean(np.argmax(teY[test_indices], axis=1) ==
-                         sess.run(predict_op, feed_dict={X: teX[test_indices],
-                                                         p_keep_conv: 1.0,
-                                                         p_keep_hidden: 1.0})))
+    # Launch the graph in a session
+    with tf.Session() as sess:
+        saver = tf.train.Saver()
+        merged = tf.summary.merge_all()
+        result_dir = './logs/attempt_{}/'.format(attempt)
+        summary_writer = tf.summary.FileWriter(result_dir, graph=sess.graph)
+        # you need to initialize all variables
+        tf.global_variables_initializer().run()
 
+        for i in range(15):
+            training_batch = zip(range(0, len(trX), batch_size),
+                                 range(batch_size, len(trX) + 1, batch_size))
+            for start, end in training_batch:
+                sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end],
+                                              p_keep_conv: 0.8, p_keep_hidden: 0.5})
+
+            saver.save(sess, 'cnn/session_{}.ckpt'.format(attempt))
+
+            test_indices = np.arange(len(teX))  # Get A Test Batch
+            np.random.shuffle(test_indices)
+            test_indices = test_indices[0:test_size]
+
+            # currently only doing 1 mini batch for testing
+            total_accuracy = []
+            test_batch_accuracy = np.mean(np.argmax(teY[test_indices], axis=1) ==
+                              sess.run(predict_op, feed_dict={X: teX[test_indices],
+                                                              p_keep_conv: 1.0,
+                                                              p_keep_hidden: 1.0}))
+            total_accuracy.append(test_batch_accuracy)
+
+            test_accuracy_summary, m_accuracy = sess.run([merged, mean_accuracy],
+                                                         feed_dict={batch_accuracies: total_accuracy})
+
+            summary_writer.add_summary(test_accuracy_summary, i + 1)
+            print("Epoch : {}, Test Acc: {}".format(i + 1, m_accuracy))
