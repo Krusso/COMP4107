73f1d33ed4c91203e76b97e9ebd90087f9bfa74f
kwojcicki <kwojcicki@sympatico.ca>
2018-12-11 15:03:00 -0500

    added basic pipeline

--- Project/code.py
+++ Project/code.py
@@ -1,5 +1,9 @@
 import math
 import tensorflow as tf
+import imblearn.over_sampling as imb
+import os
+import numpy as np
+
 
 # https://github.com/tensorflow/tensorflow/issues/6720
 # https://hackernoon.com/how-tensorflows-tf-image-resize-stole-60-days-of-my-life-aba5eb093f35
@@ -39,5 +43,183 @@ def spatial_pyramid_pool(previous_conv, num_sample, previous_conv_size, out_pool
     return spp
 
 
+def init_weights(shape):
+    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+
+
+def create_model(X, p_keep_conv, p_keep_hidden, spatial=False):
+    print("Creating model")
+    w_1 = init_weights([3, 3, 3, 32])  # 3x3x3 conv, 32 outputs
+    tf.summary.histogram("weights of first convolution layer 3x3x3x32 model5", w_1)
+
+    w_2 = init_weights([3, 3, 32, 32])  # 3x3x3 conv, 32 outputs
+    tf.summary.histogram("weights of second convolution layer 3x3x32x32 model5", w_2)
+
+    w_3 = init_weights([3, 3, 32, 32])  # 3x3x3 conv, 32 outputs
+    tf.summary.histogram("weights of third convolution layer 3x3x32x32 model5", w_3)
+
+    w_fc = init_weights([32 * 8 * 8, 625])  # FC 32 * 14 * 14 inputs, 625 outputs
+    tf.summary.histogram("weights of fully connected 625 neuron first layer model5", w_fc)
+    w_o = init_weights([625, 8])  # FC 625 inputs, 8 outputs (labels)
+    tf.summary.histogram("weights of 8 neuron output layer model5", w_o)
+
+    l1a = tf.nn.relu(tf.nn.conv2d(X, w_1,  # X=(128,32,32,3) l1a shape=(?, 32, 32, 32)
+                                  strides=[1, 1, 1, 1], padding='SAME'))
+
+    l2a = tf.nn.relu(tf.nn.conv2d(l1a, w_2,  # l1a shape=(?, 32, 32, 32) l2a shape=(?, 32, 32, 32)
+                                  strides=[1, 1, 1, 1], padding='SAME'))
+
+    l2 = tf.nn.max_pool(l2a, ksize=[1, 2, 2, 1],  # l1 shape=(?, 8, 8, 32)
+                        strides=[1, 2, 2, 1], padding='SAME')
+    l2 = tf.nn.dropout(l2, p_keep_conv)
+
+    l3a = tf.nn.relu(tf.nn.conv2d(l2, w_3,
+                                  strides=[1, 1, 1, 1], padding='SAME'))
+    l3 = tf.nn.max_pool(l3a, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
+
+    if spatial:
+        # do spatial pooling
+        print(l3.get_shape())
+        l3 = spatial_pyramid_pool(l3,
+                                  int(l3.get_shape()[1]),
+                                  [int(l3.get_shape()[2]), int(l3.get_shape()[3])],
+                                  [8, 6, 4])
+
+    l3 = tf.reshape(l3, [-1, w_fc.get_shape().as_list()[0]])  # reshape to (?, 14x14x32)
+    l3 = tf.nn.dropout(l3, p_keep_conv)
+
+    l4 = tf.nn.relu(tf.matmul(l3, w_fc))
+    l4 = tf.nn.dropout(l4, p_keep_hidden)
+
+    pyx = tf.matmul(l4, w_o)
+
+    return pyx
+
+
 # https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
 # confusion matrix
+
+
+def parse_example(example):
+    features = {'image_raw': tf.FixedLenFeature((), tf.string, default_value=""),
+                'label': tf.FixedLenFeature((), tf.int64, default_value=0)}
+    parsed_features = tf.parse_single_example(example, features)
+
+    image = tf.decode_raw(parsed_features['image_raw'], tf.float32)
+    image = tf.reshape(image, [32, 32, 3])
+
+    label = tf.cast(parsed_features['label'], tf.int64)
+    return image, tf.one_hot(label, 8)
+
+
+def input_fn(filenames, shuffle_buff=100, batch_size=128):
+    dataset = tf.data.TFRecordDataset(filenames)
+    dataset = dataset.shuffle(shuffle_buff)
+    dataset = dataset.map(lambda example: parse_example(example))
+    dataset = dataset.apply(tf.contrib.data.batch_and_drop_remainder(batch_size))
+    return dataset
+
+
+batch_size = 32
+
+train_set = input_fn(filenames=["distorted_images/train_set.tfrecords"],
+                     batch_size=batch_size)
+
+test_set = input_fn(filenames=["distorted_images/test_set.tfrecords"],
+                    batch_size=batch_size)
+# Training iterators
+train_iterator = train_set.make_initializable_iterator()
+test_iterator = test_set.make_initializable_iterator()
+
+# Pipeline for feeding into the net, gets the next batch in dataset
+next_train_batch = train_iterator.get_next()
+next_test_batch = test_iterator.get_next()
+
+# Used to reset the iterator
+train_init_op = train_iterator.initializer
+test_init_op = test_iterator.initializer
+
+X = tf.placeholder("float", [None, 32, 32, 3], name='image')
+Y = tf.placeholder("float", [None, 8], name='label')
+batch_accuracies = tf.placeholder("float", [None])
+
+p_keep_conv = tf.placeholder("float")
+p_keep_hidden = tf.placeholder("float")
+
+attempt = 0
+
+while True:
+    if os.path.isdir('./logs/attempt_{}'.format(attempt)):
+        attempt += 1
+    else:
+        break
+
+for name, model in list([("Model", create_model(X, p_keep_conv, p_keep_hidden, spatial=False))]):
+    print("Starting model", name)
+    py_x = model
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
+    train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+    predict_op = tf.argmax(py_x, 1)
+
+    mean_accuracy = tf.reduce_mean(tf.cast(batch_accuracies, tf.float32))
+
+    # Launch the graph in a session
+    with tf.Session() as sess:
+        saver = tf.train.Saver()
+        merged = tf.summary.merge_all()
+
+        testing_accuracy = tf.summary.scalar('accuracy', mean_accuracy)
+        merged_testing_accuracy = tf.summary.merge([testing_accuracy])
+
+        result_dir = './logs/attempt_{}/{}'.format(attempt, name)
+        summary_writer = tf.summary.FileWriter(result_dir, graph=sess.graph)
+
+        # you need to initialize all variables
+        tf.global_variables_initializer().run()
+
+        for epoch in range(15):
+            # Initialize the training iterator to consume training data
+            sess.run(train_init_op)
+            while True:
+                # as long as the iterator has not hit the end, continue to consume training data
+                try:
+                    images, labels = sess.run(next_train_batch)
+                    # print(images)
+                    train_summary, _ = sess.run([merged, train_op], feed_dict={X: images,
+                                                                               Y: labels,
+                                                                               p_keep_conv: 0.8, p_keep_hidden: 0.5})
+                except tf.errors.OutOfRangeError:
+                    # end of training epoch
+                    summary_writer.add_summary(train_summary, epoch)
+                    break
+
+            # Initialize the validation iterator to consume validation data
+            sess.run(test_init_op)
+            total_accuracy = []
+            while True:
+                try:
+                    images, labels = sess.run(next_test_batch)
+                    test_batch_accuracy = np.mean(np.argmax(labels, axis=1) ==
+                                                  sess.run(predict_op, feed_dict={X: images,
+                                                                                  Y: labels,
+                                                                                  p_keep_conv: 1.0,
+                                                                                  p_keep_hidden: 1.0}))
+                    total_accuracy.append(test_batch_accuracy)
+                except tf.errors.OutOfRangeError:
+                    test_accuracy_summary, m_accuracy = sess.run([merged_testing_accuracy, mean_accuracy],
+                                                                 feed_dict={batch_accuracies: total_accuracy})
+
+                    summary_writer.add_summary(test_accuracy_summary, epoch)
+                    # end of validation
+                    print("Epoch {}: testing accuracy: {}".format(epoch, m_accuracy))
+
+                    if (epoch + 1) % 5 == 0:
+                        save_path = './logs/attempt_{}/model_checkpoint/model_{}_epoch{}.ckpt'.format(attempt, name,
+                                                                                                      epoch + 1)
+                        saver.save(sess, save_path)
+                        print("Saved model at {}".format(save_path))
+                    break
+
+            summary_writer.flush()
---
+++ Project/preprocess_image.py
@@ -0,0 +1,214 @@
+import tensorflow as tf
+import sys
+import os
+import argparse
+from scipy import misc
+import numpy as np
+from urllib.request import urlretrieve
+import tarfile
+import cv2
+import glob
+from sklearn.utils import shuffle
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+FLAGS = None
+
+
+def natural_images(path='./natural_images', width=32, height=32, cropAndPad=False, short=False):
+    files = [('airplane', [1, 0, 0, 0, 0, 0, 0, 0]),
+             ('car', [0, 1, 0, 0, 0, 0, 0, 0]),
+             ('cat', [0, 0, 1, 0, 0, 0, 0, 0]),
+             ('dog', [0, 0, 0, 1, 0, 0, 0, 0]),
+             ('flower', [0, 0, 0, 0, 1, 0, 0, 0]),
+             ('fruit', [0, 0, 0, 0, 0, 1, 0, 0]),
+             ('motorbike', [0, 0, 0, 0, 0, 0, 1, 0]),
+             ('person', [0, 0, 0, 0, 0, 0, 0, 1])]
+
+    # dataset contains 6899 images
+    dataset = np.ndarray(shape=(6899, height, width, 3),
+                         dtype=np.float32)
+
+    labels = np.ndarray(shape=(6899, 8),
+                        dtype=np.float32)
+
+    with tf.Session() as sess:
+        i = 0
+        for file, label in files:
+            filepath = os.path.join(path, file)
+            filepath += '/*.jpg'
+            print(filepath)
+
+            for filename in glob.glob(filepath):
+
+                im = cv2.imread(filename, cv2.IMREAD_COLOR)
+
+                if cropAndPad:
+                    im = tf.image.resize_image_with_crop_or_pad(im, [height, width])
+                else:
+                    im = tf.image.resize_images(im, [height, width], align_corners=True)
+
+                dataset[i] = sess.run(im)
+                labels[i] = label
+                i += 1
+
+                if i % 100 == 0 and i > 0:
+                    print(i, "read")
+                    if short:
+                        break
+
+    print("Done reading images")
+
+    print("Starting to generate synthetic data")
+    # TODO: @Michael add SMOTE/ADANYS here
+
+    # Smote
+    # import imblearn.over_sampling as imb
+    # sm = imb.SMOTE(k_neighbors=1)
+    # sm = imb.ANADYS...
+    # x = [[[1, 2, 3]],
+    #      [[3, 4, 5, 6]],
+    #      [[1, 2]]]
+    # x = [[1, 2],
+    #      [1, 2],
+    #      [3, 4],
+    #      [7, 4],
+    #      [3, 6],
+    #      [4, 5]]
+    #
+    # y = [1, 1, 2, 2, 2, 2]
+    #
+    # X_train_res, y_train_res = sm.fit_sample(x, y)
+    # print(X_train_res)
+    # print(y_train_res)
+
+    print("Finished generating new data")
+
+    # trainData, trainLabel, testData, testLabel
+    # TODO: @Michael/@Krystian shuffle the data before returning it and set the bounds for training/testing correctly
+    return dataset[0:400], labels[0:400], \
+        dataset[400:600], labels[400:600]
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _write_example(image, label):
+    """
+        Args: image - a numpy array of the image
+              label - the integer value representing the image label
+        Return: tf.train.Example
+    """
+    example = tf.train.Example(
+        features=tf.train.Features(
+            feature={
+                'image_raw': _bytes_feature(image.tostring()),
+                'label': _int64_feature(label)
+            }))
+    return example
+
+
+def salt_pepper(image):
+    row, col, ch = image.shape
+
+    s_vs_p = 0.5
+    amount = 0.02
+    out = np.copy(image)
+    x = np.random.random_integers(0, row - 1, int(amount * row * col))
+    y = np.random.random_integers(0, col - 1, int(amount * row * col))
+    n = int(len(x) * s_vs_p)
+
+    # salt
+    for i, j in zip(x[:n], y[:n]):
+        # print(i, j)
+        out[i][j] = 1
+    # pepper
+    for i, j in zip(x[n:], y[n:]):
+        # print(i, j)
+        out[i][j] = 0
+    return out
+
+
+def modify_image(trX, trY):
+    # Distort the images
+    # with tf.Session() as sess:
+    # for each image, we add one distortion
+    distorted = []
+    labels = []
+    # for each image we rotate 90 degrees
+    for i in range(len(trX)):
+        # if i % 1000 == 0:
+        print("on image", i)
+
+        # d_img = sess.run(tf.image.random_flip_left_right(
+        #                trX[i].transpose(1, 2, 0))).transpose(2, 0, 1)
+        d_img = trX[i]
+
+        d_img = salt_pepper(np.rot90(d_img.transpose(1, 2, 0), np.random.randint(0, 4))).transpose(2, 0, 1)
+
+        distorted.append(d_img)
+
+        # print(d_img.shape)
+        # img = plt.imshow(d_img.transpose(1, 2, 0))
+        # plt.show()
+        # img = plt.imshow(trX[i].transpose(1, 2, 0))
+        # plt.show()
+        # break
+    distorted.extend(trX)
+    labels.extend(trY)
+    labels.extend(trY)
+    return shuffle(distorted, labels)
+
+
+DISTORTED_IMAGE_DIR = ".\distorted_images"
+if not os.path.exists(DISTORTED_IMAGE_DIR):
+    os.makedirs(DISTORTED_IMAGE_DIR)
+
+train_file = os.path.join(DISTORTED_IMAGE_DIR, 'train_set.tfrecords')
+test_file = os.path.join(DISTORTED_IMAGE_DIR, 'test_set.tfrecords')
+
+trX, trY, teX, teY = natural_images(path='./natural_images', height=32, width=32, cropAndPad=False, short=True)
+# Different sizes we can try for the height/width of the modified images
+# trX, trY, teX, teY = cifar10(path='./natural_images', height=32, width=32, cropAndPad=True)
+# trX, trY, teX, teY = cifar10(path='./natural_images', height=64, width=64, cropAndPad=False)
+# trX, trY, teX, teY = cifar10(path='./natural_images', height=64, width=64, cropAndPad=True)
+# trX, trY, teX, teY = cifar10(path='./natural_images', height=64, width=128, cropAndPad=False)
+# trX, trY, teX, teY = cifar10(path='./natural_images', height=32, width=128, cropAndPad=True)
+print("Read natural images dataset")
+
+# TODO: @Michael get the data augmentation call back into the pipeline
+# modified_images, labels = modify_image(trX, trY)
+# print("Completed Modification of Images")
+
+n = len(trX)
+train_set = []
+test_set = []
+print("Converting modified dataset to tf Examples...")
+for i in range(n):
+    # convert to tf examples, labels one-hot encoding converted to integer (later parsing will need to convert back)
+    example = _write_example(trX[i], np.where(trY[i] == 1)[0][0])
+    train_set.append(example)
+
+print("Converting testing dataset to tf Examples...")
+for i in range(len(teX)):
+    example = _write_example(teX[i], np.where(teY[i] == 1)[0][0])
+    train_set.append(example)
+
+print("Writing modified Examples into TFRecord file")
+# Rewrites all the files
+with tf.python_io.TFRecordWriter(train_file) as writer:
+    for example in train_set:
+        writer.write(example.SerializeToString())
+
+print("Writing testing Examples into TFRecord file")
+with tf.python_io.TFRecordWriter(test_file) as writer:
+    for example in train_set:
+        writer.write(example.SerializeToString())
+
+print("done")
